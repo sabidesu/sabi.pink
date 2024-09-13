6635a90e2dab3f409a385c3e99fabbf70d22e82d
sabi <sabi@sabi.pink>
2024-09-12 19:28:35 -0500

    style protogens page

--- src/pages/protogens.js
+++ src/pages/protogens.js
@@ -6,11 +6,9 @@ import { protoBg } from '../styles/proto_bg.module.css'
 const ProtogensPage = () => {
   return (
     <Layout pageTitle="">
-      <div className="card">
-        <div className="card-body">
-          <h1 className="text-info-emphasis display-1">protogens!</h1>
-          <p className="mb-0">this page should've automatically redirected you to the place where you can download the zip with the shit, but in case it didn't you can download it <a href="https://drive.google.com/file/d/1H6fxnCpdE888NsizKBDOyg6XrsUbc-TC/view?usp=drive_link" className="text-danger-emphasis text-decoration-none">here</a></p>
-        </div>
+      <div className="p-3 bg-white/80 border-4 border-cyan-700 text-slate-800">
+        <h1 className="text-cyan-700 text-5xl font-thin mb-2">protogens!</h1>
+        <p className="mb-0">this page should've automatically redirected you to the place where you can download the zip with the shit, but in case it didn't you can download it <a href="https://drive.google.com/file/d/1H6fxnCpdE888NsizKBDOyg6XrsUbc-TC/view?usp=drive_link" className="text-rose-700 no-decoration">here</a></p>
       </div>
     </Layout>
   )
