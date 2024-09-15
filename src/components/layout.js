7a79cd84e8b7a68034952912e81c4935e262940f
sabi <sabi@sabi.pink>
2024-09-15 13:15:58 -0500

    change padding on nav to match page

--- src/components/layout.js
+++ src/components/layout.js
@@ -42,10 +42,10 @@ const Navbar = ({siteTitle}) => {
   ]
 
   return (
-    <nav className="flex flex-wrap items-center justify-between sticky z-20 top-2 start-0 text-slate-800 bg-white/80 backdrop-blur-sm border-4 border-cyan-700  mx-2 sm:mx-8 sm:mt-8 p-2 sm:p-4">
-      <h6 className="text-2xl me-8">{siteTitle}</h6>
+    <nav className="flex flex-wrap items-center justify-between sticky z-20 top-2 start-0 text-slate-800 bg-white/80 backdrop-blur-sm border-4 border-cyan-700  mx-2 sm:mx-8 sm:mt-8 px-4 py-2 sm:p-4">
+      <h6 className="text-xl me-8">{siteTitle}</h6>
       <div className="flex md:order-2 space-x-3 md:space-x-0">
-        <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-rose-700 hover:text-gray-100 focus:outline-none focus:ring-2 focus:ring-rose-700" aria-controls="navbar-sticky" aria-expanded="false" onClick={toggleMenu}>
+        <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2  justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-rose-700 hover:text-gray-100 focus:outline-none focus:ring-2 focus:ring-rose-700" aria-controls="navbar-sticky" aria-expanded="false" onClick={toggleMenu}>
           <span className="sr-only">Open main menu</span>
           <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
