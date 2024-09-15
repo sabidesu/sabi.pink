8370cb0ef8077dffee201dca7c394e5de8e74011
sabi <sabi@sabi.pink>
2024-09-15 01:10:31 -0500

    rework navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,32 +12,34 @@ const Layout = ({ pageTitle, children, blogPost }) => {
     }
   `)
 
+  const navLinkStyles = "hover:text-cyan-700 transition"
+
   return (
     <div className="d-flex flex-column min-vh-100 text-netural-50">
-      <nav className="navbar navbar-expand-sm text-bg-dark fixed-top">
-        <div className="container-fluid">
-          <h6 className="navbar-brand mb-1">{data.site.siteMetadata.title}</h6>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-0">
-              <li className="nav-item"><Link to="/" className="nav-link">home</Link></li>
-              <li className="nav-item"><Link to="/about" className="nav-link">about</Link></li>
-              <li className="nav-item"><Link to="/blog" className="nav-link">blog</Link></li>
-              <li className="nav-item"><Link to="/projects" className="nav-link">projects</Link></li>
-              <li className="nav-item"><Link to="/protogens" className="nav-link">protogens!</Link></li>
-              <li className="nav-item"><Link to="/itg" className="nav-link">itg</Link></li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">tools</a>
-                <ul className="dropdown-menu">
-                  <li><Link to="/tools/pronunciation" className="dropdown-item">pronunciation</Link></li>
-                </ul>
-              </li>
+      <div className="fixed w-full z-20 top-0 start-0 text-slate-800">
+        <nav className="bg-white/80 border-4 border-cyan-700 flex flex-wrap items-center justify-between mx-2 mt-2 sm:mx-8 sm:mt-8 p-2 sm:p-4">
+          <h6 className="text-2xl me-8">{data.site.siteMetadata.title}</h6>
+          <div className="flex md:order-2 space-x-3 md:space-x-0">
+            <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+              <span className="sr-only">Open main menu</span>
+              <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+              </svg>
+            </button>
+          </div>
+          <div className="items-center justify-between hidden w-full grow md:flex md:w-auto md:order-1" id="navbar-sticky">
+            <ul className="flex flex-col p-2 md:p-0 font-medium space-y-2 sm:space-y-0 md:space-x-4 md:flex-row md:mt-0">
+              <li><Link to="/" className={navLinkStyles}>home</Link></li>
+              <li><Link to="/about" className={navLinkStyles}>about</Link></li>
+              <li><Link to="/blog" className={navLinkStyles}>blog</Link></li>
+              <li><Link to="/projects" className={navLinkStyles}>projects</Link></li>
+              <li><Link to="/protogens" className={navLinkStyles}>protogens!</Link></li>
+              {/* <li><Link to="/itg" className={navLinkStyles}>itg</Link></li> */}
+              <li><Link to="/tools/pronunciation" className={navLinkStyles}>pronunciation</Link></li>
             </ul>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
       <main className="container w-75 px-0 d-flex flex-grow-1 flex-column justify-content-center" style={{paddingTop: "4.25rem", paddingBottom: "1.5rem"}}>
         {blogPost && <Link to="/blog" className="text-light text-decoration-none">&larr; back to posts</Link>}
         <h1 className="text-cyan-700 text-5xl font-normal pb-2">{pageTitle}</h1>
