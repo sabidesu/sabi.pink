c04039c3ff62348c35d874c53587a65e4abdbce1
sabi <sabi@sabi.pink>
2024-09-15 21:57:56 -0500

    make the styling better

--- src/pages/art.js
+++ src/pages/art.js
@@ -28,7 +28,7 @@ const ArtPage = ({ data }) => {
   return (
     <Layout pageTitle="art">
       <h3 className="text-3xl mb-4">have some art i've acquired!</h3>
-      <div className="columns-lg gap-4">
+      <div className="columns-sm gap-4">
         {
           data.allContentfulArt.nodes.map((art) =>
             <ArtCard art={art} key={art.altText.altText} />
@@ -40,10 +40,15 @@ const ArtPage = ({ data }) => {
 }
 
 const ArtCard = ({art}) => {
+  const link = art.artist.link
+
   return (
-    <div className="border-2 border-cyan-700 inline-block mb-4">
+    <div className="text-pretty border-2 border-cyan-700 inline-block mb-4">
       <GatsbyImage image={art.image.gatsbyImage} alt={art.altText.altText} />
-      <p className="p-2">{art.description} / <a className="text-rose-700 hover:text-rose-400 transition" href={art.artist.link}>{art.artist.name}</a></p>
+      <p className="p-2">
+        {art.description}・
+        <a className={`${link ? "hover:text-rose-400 font-bold" : null} text-rose-700 transition`} href={link}>{art.artist.name}</a>
+      </p>
     </div>
   )
 }
