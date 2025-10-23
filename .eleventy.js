0f9fc45b7572d618c6eb0b711c26bc9dddac72ff
sabi <sabi@sabi.pink>
2025-10-23 01:07:55 -0500

    add comment explaining relative passthrough

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
 module.exports = function (eleventyConfig) {
+  // copy blog images directly for multiple formats
+  // https://www.11ty.dev/docs/copy/#copy-a-file-alongside-a-template
+  eleventyConfig.addPassthroughCopy("src/blog/**/*.jpg", {
+    mode: "html-relative",
+  });
   eleventyConfig.addPassthroughCopy("src/blog/**/*.png", {
     mode: "html-relative",
   });
