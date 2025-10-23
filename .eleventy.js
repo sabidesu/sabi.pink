32de72792d35c2dd840a31a66ea18a940d89fb2f
sabi <sabi@sabi.pink>
2025-10-23 01:31:34 -0500

    set fonts on headers

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,15 @@
 module.exports = function (eleventyConfig) {
   // copy blog images directly for multiple formats
   // https://www.11ty.dev/docs/copy/#copy-a-file-alongside-a-template
-  eleventyConfig.addPassthroughCopy("src/blog/**/*.jpg", {
+  eleventyConfig.addPassthroughCopy("./src/blog/**/*.jpg", {
     mode: "html-relative",
   });
-  eleventyConfig.addPassthroughCopy("src/blog/**/*.png", {
+  eleventyConfig.addPassthroughCopy("./src/blog/**/*.png", {
     mode: "html-relative",
   });
+
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/fonts");
 
   return {
     dir: {
